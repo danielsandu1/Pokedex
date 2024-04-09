e186aacaed52ade63124396182b2f3d0b9251db1
Daniel Sandu <daniel.sandu11@gmail.com>
2024-04-09 11:09:33 +0300

    export ui components

---
+++ components/ui/index.ts
@@ -0,0 +1,15 @@
+export { Badge, badgeVariants } from "./badge";
+export { Input } from "./input";
+export {
+  Select,
+  SelectGroup,
+  SelectValue,
+  SelectTrigger,
+  SelectContent,
+  SelectLabel,
+  SelectItem,
+  SelectSeparator,
+  SelectScrollUpButton,
+  SelectScrollDownButton,
+} from "./select";
+export { Separator } from "./separator";
