42081a10b199e88045fe1cbd2a399b85d3c2184d
Daniel Sandu <daniel.sandu11@gmail.com>
2024-04-09 12:36:24 +0300

    fixes for pokemon page

--- pages/[name]/index.tsx
+++ pages/[name]/index.tsx
@@ -5,9 +5,12 @@ import { Pokemon } from "pokenode-ts";
 
 import "@/app/globals.css";
 import { getPokemonByName } from "@/api/pokemonApi";
+import { convertHeightToMeters, convertWeightToKilograms } from "@/utils";
+import { Badge } from "@/components/ui";
+import Header from "@/components/Header";
 
 interface PokemonPageProps {
-  pokemon?: Pokemon;
+  pokemon: Pokemon;
 }
 
 const PokemonPage: React.FC<PokemonPageProps> = ({ pokemon }) => {
@@ -15,34 +18,48 @@ const PokemonPage: React.FC<PokemonPageProps> = ({ pokemon }) => {
   const { name } = router.query;
 
   return (
-    <div className="w-[40rem] mx-auto p-4 pt-8">
-      <h1 className="text-3xl font-bold mb-4">{name}</h1>
+    <div className="max-w-lg mx-auto p-4 md:pt-7">
+      <Header />
+      <h1 className="text-2xl text-center font-bold mb-4">{name}</h1>
 
-      <div className="flex items-center">
-        <div className="w-2/3 mr-4">
-          <p className="text-gray-600 font-semibold">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua.
-          </p>
+      <div className="flex items-center justify-center sm:flex-row flex-col-reverse">
+        <div className="md:w-2/3 md:mr-4">
+          <h2 className="text-lg font-semibold mb-2">Details:</h2>
+          <div className="mb-2">
+            <p className="text-gray-600">
+              <span className="font-medium">Height:</span>{" "}
+              {convertHeightToMeters(pokemon.height)}
+            </p>
+            <p className="text-gray-600">
+              <span className="font-medium">Category:</span>{" "}
+              {pokemon.species.name}
+            </p>
+            <p className="text-gray-600">
+              <span className="font-medium">Weight:</span>{" "}
+              {convertWeightToKilograms(pokemon.weight)}
+            </p>
+          </div>
+          <div>
+            <span className="font-medium">Abilities:</span>{" "}
+            {pokemon.abilities.map((ability) => (
+              <Badge
+                key={ability.ability.name}
+                className="mr-1"
+                variant="secondary"
+              >
+                {ability.ability.name}
+              </Badge>
+            ))}
+          </div>
         </div>
-        <Image
-          src={pokemon?.sprites?.front_default || ""}
-          alt={name as string}
-          className="w-1/3"
-          width={200}
-          height={200}
-        />
-      </div>
-
-      <div>
-        <h2 className="mb-2">Details:</h2>
-        <p>Height: {pokemon.height}</p>
-        <p>Category: {pokemon.species.name}</p>
-        <p>Weight: {pokemon.weight}kg</p>
-        <p>
-          Abilities:
-          {pokemon.abilities.map((ability) => ability.ability.name).join(", ")}
-        </p>
+        <div className="md:w-1/3">
+          <Image
+            src={pokemon?.sprites?.other?.home?.front_default || ""}
+            alt={name as string}
+            width={130}
+            height={130}
+          />
+        </div>
       </div>
     </div>
   );
@@ -55,6 +72,10 @@ export async function getServerSideProps(context: { query: { name: any } }) {
 
   try {
     const pokemon = await getPokemonByName(name as string);
+    if (!pokemon)
+      return {
+        notFound: true,
+      };
     return { props: { pokemon } };
   } catch (error) {
     console.error("Error fetching data:", error);
